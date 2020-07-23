fe9f9392f63e85af74ed457d840ff87f06976996
Arnaud Crowther <arnaud.crowther@dynatrace.com>
2020-07-23 08:49:55 -0400

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+import { DtIconModule } from '@dynatrace/barista-components/icon';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DtQuickFilterModule } from '@dynatrace/barista-components/experimental/quick-filter';
+import { HttpClientModule } from '@angular/common/http';
+
+@NgModule({
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        DtIconModule.forRoot({ svgIconLocation: '/assets/icons/{{name}}.svg' }),
+        DtQuickFilterModule,
+        HttpClientModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
+})
+export class AppModule {
+}
